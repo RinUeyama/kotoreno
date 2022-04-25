3362009dc2d84ad7f1f3d88a4ecccc77d900599f
Opion <KindledKindred@gmail.com>
2022-04-25 12:30:58 +0900

    feat: タイトル放り込んだだけ

--- src/components/top/TopHero.tsx
+++ src/components/top/TopHero.tsx
@@ -1,14 +1,20 @@
 import * as PIXI from 'pixi.js'
 import { Suspense, useEffect, useState } from 'react'
 import { createPixiApp } from "../../utils/pixiUtil"
+import style from './TopHero.module.css'
 
 type TopHeroProps = {}
 
 const TopHero: React.FC<TopHeroProps> = ({}) => {
-  const onLoad = (element: HTMLDivElement) => {
-    if (typeof element === 'undefined') return;
-    const app = createPixiApp(element);
+  useEffect(() => {
+    const loader = new PIXI.Loader()
+    loader.add("topHero", "./assets/topHero.png")
+  }, [])
 
+  const onLoad = (element: HTMLDivElement) => {
+    if (!element) return;
+    const app = createPixiApp(element);
+    
     const topHero = PIXI.Sprite.from("./assets/topHero.png")
     topHero.x = 0
     topHero.y = 0
@@ -18,14 +24,13 @@ const TopHero: React.FC<TopHeroProps> = ({}) => {
     app.stage.addChild(topHero)
   }
 
-  useEffect(() => {
-    const loader = new PIXI.Loader()
-    loader.add("topHero", "./assets/topHero.png")
-  }, [])
-
   return (
     <Suspense fallback={<div>TODO: Loading</div>}>
-      <div ref={onLoad} />
+      <div aria-label="タイトル　ことれの" ref={onLoad} />
+      <h1 className={style.title}>
+        <span className={style.head}>ことれの</span>
+        <span className={style.tail}>奇譚</span>
+      </h1>
     </Suspense>
   )
 }
